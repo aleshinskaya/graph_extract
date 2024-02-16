b5b3453a1508794e08da3ea28ad2505f697c3c6f
aleshinskaya <anna.leshinskaya@gmail.com>
2024-02-15 23:34:48 -0800

    edits to translate.py

--- translate.py
+++ translate.py
@@ -15,13 +15,33 @@ config = dotenv_values(".env")
 
 CUR_DIR = os.path.dirname(os.path.abspath(__name__))
 DATA_DIR = CUR_DIR+'/data/'
-VIS_JS_FORMAT = "{id: %d, label: '%s'}"
-VIS_JS_FORMAT_LINK = "{from: %d, to: %d, label: '%s'}"
+VIS_JS_FORMAT_NODE = "{id: %d, label: '%s', color: {background: '%s'}}"
+VIS_JS_FORMAT_LINK = "{from: %d, to: %d, label: '%s', arrows: { to: { enabled: true, type: 'arrow'}}}"
+
+# color: {
+#       border: '#2B7CE9',
+#       background: '#97C2FC',
+#       highlight: {
+#         border: '#2B7CE9',
+#         background: '#D2E5FF'
+#       },
+
+# arrows: {
+#       to: {
+#         enabled: false,
+#         imageHeight: undefined,
+#         imageWidth: undefined,
+#         scaleFactor: 1,
+#         src: undefined,
+#         type: "arrow"
+#       },
+
+NODE_COLORS = {'being': '#279aba', 'action_choice': '#ba2769', 'event': '#e6c440'}
 
 HEADER = "<html>\n<head>\n<script type='text/javascript'\
-    \nsrc='https://unpkg.com/vis-network/standalone/umd/vis-network.min.js'></script>\
-    \n\t<style type='text/css'>\
-      \n  #mynetwork {\
+src='https://unpkg.com/vis-network/standalone/umd/vis-network.min.js'></script>\
+    \n<style type='text/css'>\
+      \n #mynetwork {\
         \n\t\twidth: %dpx;\
          \n\t\theight: %dpx;\
          \n\t\tborder: 1px solid lightgray;\
@@ -34,7 +54,7 @@ HEADER = "<html>\n<head>\n<script type='text/javascript'\
 
 BODY = '\n<script type="text/javascript">\
     \n\nwindow.onload = function(){\
-       \n\t\tlaunch_visualization()\
+       \n\tlaunch_visualization()\
     \n}\
     \nfunction launch_visualization() { \
         \n\tthis_dset =  return_vis_dataset()\
@@ -46,8 +66,32 @@ BODY = '\n<script type="text/javascript">\
             \n\t\tnodes: nodes,\
             \n\t\t edges: edges\
          \n\t\t};\
-    \n\tvar options = {};\
-\
+    \n\tvar options = {\
+		\n\t\tphysics: {\
+			\n\t\t\tenabled: true,\
+			\n\t\t\tbarnesHut: {\
+			\n\t\t\ttheta: 0.5,\
+			\n\t\t\tgravitationalConstant: -5000,\
+			\n\t\t\tspringLength: 500,\
+			\n\t\t\tspringConstant: 0.01,\
+			\n\t\t\tdamping: 5,\
+			\n\t\t\tavoidOverlap: 1\
+			\n\t\t}\
+		\n\t},\
+		\n\t\tmanipulation: {\
+			\n\t\t\tenabled: true,\
+			\n\t\t\tinitiallyActive: false,\
+			\n\t\t\taddNode: true,\
+			\n\t\t\taddEdge: true,\
+			\n\t\t\teditEdge: true,\
+			\n\t\t\tdeleteNode: true,\
+			\n\t\t\tdeleteEdge: true,\
+		\n\t\t\t},\
+		\n\t\tnodes: {\
+			\n\t\t\tshape: "circle",\
+            \n\t\t\tfont: { size: 16 },\
+            \n\t\t\t}\
+	}; \
        \n // initialize your network!\
          \n\tvar network = new vis.Network(container, data, options);\
     \n}\n'
@@ -59,8 +103,16 @@ CODA = '\n</script>\
 
 
 
-json_file = 'json_output_2.jsonl';
-main(json_file)
+# json_file = 'json_output_2.jsonl'
+# main(json_file)
+
+
+def wrap_text(txt,width=5):
+    txt_split =  txt.split(' ')
+    txt_wrapped = [(' '.join(txt_split[i:i+width])) for i in range(0, len(txt_split), width)]
+    txt_complete = '\\n '.join(txt_wrapped)
+    # print(txt_complete)
+    return (txt_complete)
 
 def main(json_file: str = typer.Option(None, help="name of json file to use")):
 
@@ -73,8 +125,8 @@ def main(json_file: str = typer.Option(None, help="name of json file to use")):
     #graph size
     n_nodes = len(node_list)
 
-    width = n_nodes * 80
-    height = n_nodes * 80
+    width = n_nodes * 120
+    height = n_nodes * 120
 
 
 
@@ -96,7 +148,15 @@ def main(json_file: str = typer.Option(None, help="name of json file to use")):
     for ind,item in enumerate(node_list):        
         
         this_lab = item['node']['label']
-        this_line = VIS_JS_FORMAT % (ind+1, this_lab)
+        this_node_kind = item['node']['kind']
+        try:
+            this_color = NODE_COLORS[this_node_kind]
+        except:
+            this_color = '#62a3a2'
+
+        this_lab_wrapped = wrap_text(this_lab)
+        this_line = VIS_JS_FORMAT_NODE % (ind+1, this_lab_wrapped,this_color)
+        # print(this_line)
         node_labs_list[this_lab ] = ind
         if(ind<(len(node_list)-1)):
             this_line=this_line+',\n\t\t'
@@ -109,13 +169,16 @@ def main(json_file: str = typer.Option(None, help="name of json file to use")):
 
     # print(node_list)
 
+    # color: { border: '#280a2b',  background: '#9b32a8'}, font: {color: '#f7f0f7',
+    #   size: 14, face: 'arial'}
+
     # create list of edges for each node
     # loop through each node and write out each of its links!
 
 
     for node_ind,item in enumerate(node_list):        
         
-        this_node_lab = item['node']['label']        
+        this_node_lab = item['node']['label']
         these_links = item['links'] 
         
 
@@ -126,7 +189,7 @@ def main(json_file: str = typer.Option(None, help="name of json file to use")):
         if(is_last_node and len(these_links)==0):
             output_file.write('\n\t\t]);')
         elif(is_first_node==False and len(these_links)>0):
-            output_file.write(',\n\t\t')
+            output_file.write('\n\t\t')
 
         for link_ind,link in enumerate(these_links):
 
