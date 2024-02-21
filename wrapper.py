2a0012ad6810e3455b8cab12a4b4d5f907a5b865
aleshinskaya <anna.leshinskaya@gmail.com>
2024-02-21 13:52:06 -0800

    error handling in wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -10,19 +10,32 @@ importlib.reload(annotate_scenario)
 CUR_DIR = os.path.dirname(os.path.abspath(__name__))
 DATA_DIR = CUR_DIR+'/data/'
 
-def main(filename = "scenarios.json", scenario_id: int = 0):
+def main(filename: str = 'scenarios.json"', scenario_id: int = 0):
 
-    with open(DATA_DIR+"scenarios.json", 'r') as file:
+    with open(DATA_DIR+filename, 'r') as file:
         scenarios=json.load(file)
 
-    this_scenario_text = scenarios[scenario_id]["text"]
-    
+    # error handling for assumptions about json entries
+    try:
+        scenario_json = scenarios[scenario_id]
+    except:
+        # print("Check scenario filename or scenario id")
+        raise IndexError('Check scenario id exists in json file!')
+
+    assert isinstance(scenario_json['id'],int)
+    assert scenario_json['text']
+    assert scenario_json['options']
+
+    # display the scnenario text read in 
+    this_scenario_text = scenario_json["text"]    
     print('Scenario Text: \n\n')
     print(textwrap.fill(this_scenario_text, width = 100), '\n\n')
 
+    # generate output file name based on input filename
     output_filename = filename.split('.json')[0]+'_'+str(scenario_id)
 
-    annotate_scenario.main(scenarios[scenario_id],output_filename)
+    # run the annotation process
+    annotate_scenario.main(scenario_json,output_filename)
 
 
 if __name__ == "__main__":
